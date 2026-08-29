51f093893dc6f7c652e26841bf68d2f201c5c804
Khrisna Gunanasurya <khrisnagunanasurya@gmail.com>
2026-08-29 18:28:07 +0700

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.go
@@ -0,0 +1,13 @@
+func longestCommonPrefix(strs []string) string {
+	first := strs[0]
+
+	for pos := 0; pos < len(first); pos++ {
+		for _, word := range strs[1:] {
+			if pos >= len(word) || word[pos] != first[pos] {
+				return first[:pos]
+			}
+		}
+	}
+
+	return first
+}	
